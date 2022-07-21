590ea71cfca2ae105904fbe08b3860e50b658662
Claudi0-V <claudiov.dejesus@protonmail.com>
2022-07-21 18:09:10 -0300

    complete overhaul

--- script.js
+++ script.js
@@ -1,163 +1,247 @@
+const sleep = async (ms = 500) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
 const board = (() => {
-  let _board = ['', '', '', '', '', '', '', '', ''];
-  const winningSets = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8],[0,4,8], [2,4,6]];
+  let _board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const winningSets = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
   const getBoard = () => [..._board];
-  const isInBoard = index => !!_board[index];
-  const setInBoard = (index, playerHand) => _board[index] = playerHand;
-  const resetBoard = () => _board = ['', '', '', '', '', '', '', '', ''];
-  const testWin = (currentPlay) => winningSets.some(arr => 
-                        arr.every(index => _board[index] === currentPlay));
-  return {setInBoard, isInBoard, resetBoard, testWin, getBoard,};
+  const isInBoard = (index) => !!_board[index];
+  const setInBoard = (index, playerHand) => (_board[index] = playerHand);
+
+  const avaliableIndexes = (board = _board) => {
+    return board
+      .map((elem, index) => (!elem ? index : 0))
+      .filter((elem) => elem);
+  };
+
+  const resetBoard = () => (_board = [0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+  const isAWin = (currentPlay) => {
+    return winningSets.some((arr) =>
+      arr.every((index) => _board[index] === currentPlay)
+    );
+  };
+
+  return {
+    setInBoard,
+    isInBoard,
+    resetBoard,
+    isAWin,
+    getBoard,
+    avaliableIndexes,
+  };
 })();
 
-const Player = (name, XorO) => {
-  let _player = name;
-  let _sign = XorO;
-  let _wins = 0
+const Player = (name, sign) => {
+  const _name = name;
+  const _sign = sign;
+  const _type = "Player";
+  let _wins = 0;
 
-  const getName = () => _player;
+  const getName = () => _name;
   const getSign = () => _sign;
-  const setWins = () => _wins++;
+  const increaseWins = () => _wins++;
+  const getType = () => _type;
   const getWins = () => _wins;
-  return  {getName, getSign, getWins, setWins}
-}
 
-const AI = () => {
-  let _sign = 'O'
-  let _wins = 0
+  return { getName, getSign, getWins, increaseWins, getType };
+};
 
-  const getName = () => "AI";
+const AI = (name, sign) => {
+  const _name = name;
+  const _sign = sign;
+  const _type = "AI";
+  let _wins = 0;
+
+  const getName = () => _name;
   const getSign = () => _sign;
-  const setWins = () => _wins++;
+  const increaseWins = () => _wins++;
   const getWins = () => _wins;
-  const play = () => {
-    let hand;
-    do { 
-      hand = Math.floor(Math.random() * (8 - 0)) 
-    } 
-    while(board.isInBoard(hand));
-    return hand
-  }
-  return  {getName, getSign, getWins, setWins, play} 
-}
+  const getType = () => _type;
+
+  const play = (currentBoard = board) => {
+    const avaliableIndexes = currentBoard.avaliableIndexes();
+    const hand = Math.floor(Math.random() * avaliableIndexes.length);
+    return avaliableIndexes[hand];
+  };
+
+  return { getName, getSign, getWins, increaseWins, play, getType };
+};
 
 const gameUI = (() => {
-  const displayInfo = document.querySelector('.display-info');
-  const gameCell = document.querySelectorAll('.game-cell');
-  const continueGame = document.querySelector('#continue-game-button');
-  const mainGame = document.querySelector('.main-game');
-  const mainContainer = mainGame.parentNode;
-  const sendPlayersInfo = document.querySelector('.send-players-info');
-  const newGame = document.querySelector('#new-game-button')
+  const $ = document.querySelector.bind(document);
+  const $$ = document.querySelectorAll.bind(document);
 
-  const winUI = (sign) => {
-  if (board.testWin(sign)) {
-     gameUI.displayAlert('win',`${game.sendCurrentPlayer().getName()} Won!`);
-     game.sendCurrentPlayer().setWins();
-     board.resetBoard();
-     setTimeout(() => game.endGame(`Winner: ${game.sendCurrentPlayer().getName()}`), 500)
-}}
-  const winnerUI = (text) => document.querySelector('.winner').textContent = text;
-  const playersUI = (arr) => { 
-      let players = document.querySelectorAll('.players');
-      let playersWins = document.querySelectorAll('.player-win-count');
-      players.forEach((player, index) => player.textContent = arr[index].getName());
-      playersWins.forEach((win, index) => win.textContent = arr[index].getWins());
-  }
-  const resetInterface = () => gameCell.forEach(cell => cell.textContent = '');
-  const displayAlert = (type, message) => {
-    let alertDiv = document.createElement('div');
+  const displayInfo = $(".display-info");
+  const gameCells = $$(".game-cell");
+  const continueGame = $("#continue-game-button");
+  const mainGame = $(".main-game");
+  const mainContainer = mainGame.parentNode;
+  const playersInfoForm = $(".players-info-container");
+  const newGame = $("#new-game-button");
+
+  const winUI = async (player) => {
+    await sleep();
+    game.endGame(`Winner: ${player.getName()}`);
+  };
+
+  const winnerUI = (text) => ($(".winner").textContent = text);
+
+  const playersUI = (arr) => {
+    const players = $$(".players");
+    const playersWins = $$(".player-win-count");
+    players.forEach(
+      (player, index) => (player.textContent = arr[index].getName())
+    );
+    playersWins.forEach(
+      (win, index) => (win.textContent = arr[index].getWins())
+    );
+  };
+
+  const resetInterface = () =>
+    gameCells.forEach((cell) => {
+      cell.textContent = "";
+      cell.classList.remove("puff-in-center");
+    });
+
+  const displayAlert = async (type, message) => {
+    const alertDiv = document.createElement("div");
     alertDiv.textContent = message;
-    alertDiv.classList.add(`${type}`, 'alert');
+    alertDiv.classList.add(`${type}`, "alert");
     mainContainer.insertBefore(alertDiv, mainGame);
-    setTimeout(() => document.querySelector('.alert').remove(), 1500);
-  }
+    await sleep(1500);
+    $(".alert").remove();
+  };
+
   const displayEndGame = (text) => {
     winnerUI(text);
-    playersUI(game.SendPlayers())
-    displayInfo.style.display = 'flex';
-  }
-  sendPlayersInfo.addEventListener('click', () => {
-      let p1 = document.querySelector('#p1-name')
-      let p2 = document.querySelector('#p2-name')
-      game.setPlayersName(p1.value, 'X', p2.value, 'O');
-      document.querySelector('.players-info-container').style.display = 'none'
-    })
+    playersUI(game.sendPlayers());
+    displayInfo.style.display = "flex";
+  };
 
+  playersInfoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
+    const p1 = $("#p1-name").value;
+    const p2 = $("#p2-name").value;
+    const p2Type = $('input[name="p2Choice"]:checked').value;
 
-  gameCell.forEach(cell => cell.addEventListener('click', e => {
-    let index = parseInt(e.target.dataset.value);
-    let currentSing;
-    if (board.isInBoard(index)) displayAlert("danger", "you can't play there!");
-    else {
-      let thisSing = game.sendCurrentPlayer().getSign();
-      currentSing = thisSing;
-      game.singleRound(index, thisSing);
-      e.target.textContent = thisSing;
-      let gameRound = game.sendRound();
-      let possibleAI = game.SendPlayers()[1]
-      winUI(currentSing)
-      if (possibleAI.getName() === 'AI' && gameRound < 9) {
-        game.changeCurrentPlayer()
-        let aiHand = possibleAI.play();
-        let aiIndex = document.querySelector(`[data-value="${aiHand}"]`);
-        game.singleRound(aiHand, possibleAI.getSign());
-        currentSing = possibleAI.getSign();
-        aiIndex.textContent = possibleAI.getSign();
-        winUI(currentSing)
-      };
-      game.changeCurrentPlayer()
-      if(gameRound >= 9) {
-      gameUI.displayAlert('danger',`It's a Draw`);
-      board.resetBoard();
-      setTimeout(() => game.endGame("It's a Draw"), 500)
-      }
-    }
-  }));
-
-  continueGame.addEventListener('click', () => {
-    resetInterface();
-    displayInfo.style.display = 'none';
-  })
-
-  newGame.addEventListener('click', () => { 
-      displayInfo.style.display = 'none';
-      document.querySelector('.players-info-container').style.display = "flex";
-      resetInterface();
+    game.setPlayersName(p1, "X", p2, "O", p2Type);
+    e.target.style.display = "none";
   });
-  return {resetInterface, displayAlert, displayEndGame, playersUI,};
+
+  gameCells.forEach((cell, index) =>
+    cell.addEventListener("click", (event) => {
+      if (board.isInBoard(index)) {
+        return gameUI.displayAlert("danger", "you can't play there!");
+      }
+      game.gameFlow(event, index);
+    })
+  );
+
+  continueGame.addEventListener("click", () => {
+    board.resetBoard();
+    resetInterface();
+    displayInfo.style.display = "none";
+  });
+
+  newGame.addEventListener("click", () => {
+    displayInfo.style.display = "none";
+    $(".players-info-container").style.display = "flex";
+    game.resetGame();
+    resetInterface();
+  });
+
+  return { resetInterface, displayAlert, displayEndGame, playersUI, winUI };
 })();
 
 const game = (() => {
-  let player1 = Player('Player 1', 'X');
-  let player2 = AI();
-  let round = 0;
-  let currentPlayer = player1;
-  
-  const sendRound = () => round;
-  const sendCurrentPlayer = () => currentPlayer;
-  const changeCurrentPlayer = () => currentPlayer = currentPlayer === player2 ? player1 : player2;
+  let player1;
+  let player2;
+  let _round = 0;
+  let _currentPlayer;
+
+  const getRound = () => _round;
+  const sendCurrentPlayer = () => _currentPlayer;
+  const sendPlayers = () => [player1, player2];
+
+  const changeCurrentPlayer = () =>
+    (_currentPlayer = _currentPlayer === player2 ? player1 : player2);
+
   const resetGame = () => {
-    round = 0;
-  }
-  const setPlayersName = (p1Name, p1Sing, p2Name, p2Sing ) => {
-    player1 = Player(p1Name, p1Sing);
-    player2 = p2Name === 'AI' ? AI() : Player(p2Name, p2Sing);
-    gameUI.playersUI(game.SendPlayers());
-  }
-  const SendPlayers = () => [player1, player2];
+    board.resetBoard();
+    _round = 0;
+  };
+
+  const setPlayersName = (p1Name, p1Sing, p2Name, p2Sing, p2type) => {
+    player1 = Player("👨‍🦱" + p1Name, p1Sing);
+    _currentPlayer = player1;
+    const isAI = p2type === "AI";
+    player2 = isAI ? AI(p2Name + "🤖", p2Sing) : Player(p2Name + "👨‍🦱", p2Sing);
+    gameUI.playersUI(game.sendPlayers());
+  };
+
   const endGame = (text) => {
     resetGame();
-    gameUI.displayEndGame(text)
+    gameUI.displayEndGame(text);
   };
-  const singleRound = (targetIndex, sign) => {
-    const index = targetIndex;
-    board.setInBoard(index, sign);
-    round++
-    ;
-  }
 
-  return {singleRound,sendCurrentPlayer,resetGame, endGame, SendPlayers, setPlayersName, sendRound, changeCurrentPlayer,}
+  const singleRound = (targetIndex, sign) => {
+    board.setInBoard(targetIndex, sign);
+    _round++;
+  };
+
+  const hasAI = () => player1.getType() === "AI" || player2.getType() === "AI";
+
+  const gameFlow = async ({ target }, index, player) => {
+    const currPlayer = player || game.sendCurrentPlayer();
+    const sign = currPlayer.getSign();
+    game.singleRound(index, sign);
+    target.classList.add("puff-in-center");
+    target.textContent = sign;
+    await sleep();
+
+    if (board.isAWin(sign)) {
+      currPlayer.increaseWins();
+      return gameUI.winUI(currPlayer);
+    }
+
+    if (game.getRound() === 9) {
+      board.resetBoard();
+      await sleep();
+      return game.endGame("It's a Draw");
+    }
+
+    if (game.hasAI() && !player) {
+      const aiHand = player2.play();
+      const aiIndex = document.querySelector(`[data-value="${aiHand}"]`);
+      return gameFlow({ target: aiIndex }, aiHand, player2);
+    }
+    if (!game.hasAI()) {
+      game.changeCurrentPlayer();
+    }
+  };
+
+  return {
+    singleRound,
+    sendCurrentPlayer,
+    resetGame,
+    endGame,
+    sendPlayers,
+    setPlayersName,
+    getRound,
+    changeCurrentPlayer,
+    gameFlow,
+    hasAI,
+  };
 })();
